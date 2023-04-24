c763afadb044e4aaf010c8ec56e84d7e463804a4
handshou <handshou@users.noreply.github.com>
2023-04-24 17:50:18 +0800

    Add site config
    More config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,16 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({});
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://tarot.hansel.co',
+  integrations: [mdx(), sitemap()],
+  output: "server",
+  adapter: cloudflare()
+});
